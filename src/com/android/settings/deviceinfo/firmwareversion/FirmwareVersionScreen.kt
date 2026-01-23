69c62422512db83e1bf55aedb7750525918d8f21
MnzzPrjkt <mnzzprjkt@gmail.com>
2026-01-23 01:41:16 +0000

    add: Maintainer Name
    Change-Id: I534d61d756c2ef3f9fc935cdc0b119da0aa36a17

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -72,6 +72,7 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
             +FirmwareVersionDetailPreference()
             +SecurityPatchLevelPreference()
             +MainlineModuleVersionPreference()
+            +CustomMaintainerPreference()
             +BasebandVersionPreference()
             +KernelVersionPreference()
             +SimpleBuildNumberPreference()
